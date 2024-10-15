cf6febd2be6cdfa83dcc2630acd2c88c5c349890
tiagoprogwebt2 <tiago.progweb2024t2@gmail.com>
2024-10-15 14:44:16 -0300

    Create 01-hello-world.php

---
+++ 01-hello-world.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iniciando com PHP</title>
+</head>
+<body>
+    <h1>Experiências com PHP</h1>
+    <hr>
+
+    <!-- Delimitador de abertura -->
+    <?php
+
+    ?>
+
+</body>
+</html>
