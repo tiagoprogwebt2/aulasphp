6ca1912804642cc1a2b365ff77ed1b03b2102ae2
tiagoprogwebt2 <tiago.progweb2024t2@gmail.com>
2024-10-16 16:43:26 -0300

    Arrays

--- 04-arrays.php
+++ 04-arrays.php
@@ -18,6 +18,35 @@ $cursos = array("Node.js", "Design", "Nutrição");
         <h2>Acessando os dados do array</h2>
         <p>Rock Progressivo: <?=$bandas[2]?> </p>
         <p>Ontem estivemos com a turma de <?=$cursos[2]?></p>
+
+        <hr>
+
+<?php
+// Array associativo
+$filme = [
+    // chave associativa
+    "titulo" => "Senhor dos Anéis",
+    "autor" => "J.R.R. Tolkien",
+    "lancamento" => 2001,
+    "genero" => "Fantasia"
+];
+?>
+        <h2>Acessando os dados do array associativo</h2>
+        <p>O filme <?=$filme["titulo"]?> foi lançado
+        em <?=$filme["lancamento"]?>. </p>
+
+        <hr>
+
+        <h2>Comandos úteis (para programadores) para análise de arrays</h2>
+
+        <h3>print_r(nomeArray)</h3>
+        <pre> <?=print_r($filme)?> </pre>
+        
+        <h3>var_dump(nomeArray)</h3>
+        <pre> <?=var_dump($filme)?> </pre>
+
     </div>
+
+    
 </body>
 </html>
