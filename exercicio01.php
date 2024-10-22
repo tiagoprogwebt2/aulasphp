d4a6272bde9cf1bb2e581e1359917b9cbab60394
tiagoprogwebt2 <tiago.progweb2024t2@gmail.com>
2024-10-22 14:29:54 -0300

    Correção: criando o array

---
+++ exercicio01.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Exercício 01 (corrigido)</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+        <h1>Exercício 01 (corrigido)</h1>
+        <hr>
+
+<?php
+    // Array NUMÉRICO/INDEXADO
+    $dados1 = [
+        "chapolin.colorado", 
+        "123teste", 
+        30, 
+        "São Paulo",
+        ["11-2135-0300","11-91234-5678"]
+    ];
+
+
+    // Array ASSOCIATIVO
+    $dados2 = [
+        "usuario" => "chapolin.colorado",
+        "senha" => "123teste",
+        "idade" => 30,
+        "cidade" => "São Paulo",
+        "telefones" => ["11-2135-0300", "11-98765-4321"]
+    ];
+?>
+
+    </div>
+</body>
+</html>
