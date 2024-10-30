260e4e7c6de70bf9fcccce61a50f7cf5eccbf74c
tiagoprogwebt2 <tiago.progweb2024t2@gmail.com>
2024-10-30 17:30:03 -0300

    Update processamento.php

--- processamento.php
+++ processamento.php
@@ -16,15 +16,37 @@ $nome = $_POST["nome"];
 $email = $_POST["email"];
 $nascimento = $_POST["nascimento"];
 $mensagem = $_POST["mensagem"];
+
+/* Se um dos campos obrigatórios estiver vazio */
+if( empty($nome) || empty($email) ){
+?>
+    <!-- Mostramos um alerta para o usuário -->
+    <p style="color: red;">
+        Você DEVE preencher <b>nome</b> e <b>e-mail</b>!
+    </p>
+<?php
+} else {
 ?>
     <h2>Dados recebidos</h2>
     <ul>
         <li>Nome: <?=$nome?></li>
         <li>E-mail: <?=$email?></li>
+        
+        <!-- Verificando se os campos
+         nascimento e mensagem NÃO ESTÃO VAZIOS.
+         Note o uso do operador ! para inverter
+         a lógica da função empty() -->
+
+        <?php if( !empty($nascimento) ){ ?>
         <li>Data de nascimento: <?=$nascimento?></li>
+        <?php } ?>
+        
+        <?php if( !empty($mensagem) ){ ?>
         <li>Mensagem: <?=$mensagem?></li>
+        <?php } ?>
     </ul>
 
+<?php } ?>
 
 
 </body>
